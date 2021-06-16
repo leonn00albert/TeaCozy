89d4315cc24eaeb82bf3798133c118cc83cfa3c6
Your Name <you@example.com>
2021-06-16 16:30:19 +0400

    fix paths

--- src/components/TeaSection/TeaSection.js
+++ src/components/TeaSection/TeaSection.js
@@ -3,11 +3,11 @@ export const TeaSection = () => {
     width: "1000px",
   };
   const images = [
-    { src: "/img/img-berryblitz.jpg", text: "Fall Berry Blitz Tea" },
-    { src: "/img/img-spiced-rum.jpg", text: "Spiced Rum Tea" },
-    { src: "/img/img-donut.jpg", text: "Seasonal Donuts" },
-    { src: "/img/img-myrtle-ave.jpg", text: "Myrtal Ave Tea" },
-    { src: "/img/img-bedford-bizarre.jpg", text: "Bedford Bizarre Tea" },
+    { src: "./img/img-berryblitz.jpg", text: "Fall Berry Blitz Tea" },
+    { src: "./img/img-spiced-rum.jpg", text: "Spiced Rum Tea" },
+    { src: "./img/img-donut.jpg", text: "Seasonal Donuts" },
+    { src: "./img/img-myrtle-ave.jpg", text: "Myrtal Ave Tea" },
+    { src: "./img/img-bedford-bizarre.jpg", text: "Bedford Bizarre Tea" },
   ];
 
   return (
